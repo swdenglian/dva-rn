2f097c5a8459e66539ad1990c522484318db976f
swdenglian <denglian1008@163.com>
2019-08-09 09:59:35 +0800

    export createHashHistory

--- packages/dva-rn/src/index.tsx
+++ packages/dva-rn/src/index.tsx
@@ -1,5 +1,12 @@
 import Dva, { routerRedux, createBrowserHistory, IRouterConfigs } from "./dva";
+import { createHashHistory } from "history";
 
 export { IModel } from "./dva/dva-types";
-export { Dva, routerRedux, createBrowserHistory, IRouterConfigs };
+export {
+  Dva,
+  routerRedux,
+  createBrowserHistory,
+  createHashHistory,
+  IRouterConfigs
+};
 export { connect } from "react-redux";
